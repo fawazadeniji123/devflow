104026a63d171ba1164a2993580e9f0cdd92fc56
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-09-09 12:30:12 +0100

    setup clerk auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,19 @@
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <h1 className="h1-bold">Next.js 14 Here we come</h1>
     </div>
   );
